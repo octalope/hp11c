47df093585d1cc87686e4be64e13af9b233df2bf
Steve Brooks <steve.c.brooks@gmail.com>
2020-02-09 22:09:09 -0500

    simplifiy init

--- src/index.js
+++ src/index.js
@@ -43,44 +43,35 @@ const processKey = (data) => {
   updateDisplay();
 };
 
-
 window.onload = (event) => {
   display = document.querySelector('div[name="display0"]');
   updateDisplay();
 
-  // listen for number keys
+  // number key handlers
   for (let i = 0; i < 10; i++) {
     document.querySelector(`div[name="${i}"]`)
       .addEventListener('click', processKey.bind(null, {op: 'insertChar', char: String(i)}));
   }
 
-  document.querySelector('div[name="deleteChar"]')
-    .addEventListener('click', processKey.bind(null, {op: 'deleteChar'}));
+  // button/operation map
+  const buttonMap = {
+    changeSign: 'changeSign',
+    deleteChar: 'deleteChar',
+    enterDecimal: 'enterDecimal',
+    enterExponent: 'enterExponent',
+    enter: 'enter',
+    add: 'add',
+    subtract: 'subtract',
+    multiply: 'multiply',
+    divide: 'divide',
+  };
 
-  document.querySelector('div[name="changeSign"]')
-    .addEventListener('click', processKey.bind(null, {op: 'changeSign'}));
-
-  document.querySelector('div[name="enterDecimal"]')
-    .addEventListener('click', processKey.bind(null, {op: 'enterDecimal'}));
-
-  document.querySelector('div[name="enterExponent"]')
-    .addEventListener('click', processKey.bind(null, {op: 'enterExponent'}));
-
-  document.querySelector('div[name="enter"]')
-    .addEventListener('click', processKey.bind(null, {op: 'enter'}));
-
-  // document.querySelector('div[name="drop"]')
-  //   .addEventListener('click', processKey.bind(null, {op: 'drop'}));
-
-  document.querySelector('div[name="add"]')
-    .addEventListener('click', processKey.bind(null, {op: 'add'}));
-
-  document.querySelector('div[name="subtract"]')
-    .addEventListener('click', processKey.bind(null, {op: 'subtract'}));
-
-  document.querySelector('div[name="multiply"]')
-    .addEventListener('click', processKey.bind(null, {op: 'multiply'}));
-
-  document.querySelector('div[name="divide"]')
-    .addEventListener('click', processKey.bind(null, {op: 'divide'}));
+  for (let btn in buttonMap) {
+    if (Object.prototype.hasOwnProperty.call(buttonMap, btn)) {
+      document.querySelector(`div[name="${btn}"]`)
+        .addEventListener('click', processKey.bind(null, {op: buttonMap[btn]}));
+    } else {
+      console.error(`Failed to add event listener for button: ${btn}`);
+    }
+  }
 };
